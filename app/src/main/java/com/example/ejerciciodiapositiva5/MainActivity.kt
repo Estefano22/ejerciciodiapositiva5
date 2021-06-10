20debc5f7c414e72e084544d596ae452ffc82159
Estefano22 <sandulescustefano8@gmail.com>
2021-06-10 09:36:43 +0200

    hecho mas o menos bien

--- app/src/main/java/com/example/ejerciciodiapositiva5/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciodiapositiva5/MainActivity.kt
@@ -1,37 +1,52 @@
 package com.example.ejerciciodiapositiva5
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ejerciciodiapositiva5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        button.isEnabled = false
+        binding.button.isEnabled=false
 
+        binding.editText1.addTextChangedListener(getTextWatcher())
+        binding.editText2.addTextChangedListener(getTextWatcher())
 
-        EditText1.addTextChangedListener(getTextWatcher())
-        editText2.addTextChangedListener(getTextWatcher())
+        binding.button.setOnClickListener{
+            binding.editText1.text.append(binding.editText2.text.toString())
+            binding.editText2.text.clear()
+        }
 
-        button.setOnClickListener{
-            editText1.text.append(editText2.text.toString())
-            editText2.text.clear()
+        binding.button.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) binding.textView.text = binding.button.tag.toString()
+        }
+
+        binding.editText1.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) binding.textView.text = binding.editText1.tag.toString()
+        }
+
+        binding.editText2.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) binding.textView.text = binding.editText2.tag.toString()
         }
 
     }
 
-    private fun enableButton() {
-        button.isEnabled = editText1.text.isNotEmpty() && editText2.text.isNotEmpty()
+    private fun habilitarButton() {
+        binding.button.isEnabled = binding.editText1.text.isNotEmpty() && binding.editText2.text.isNotEmpty()
     }
 
     private fun getTextWatcher() : TextWatcher {
 
         return object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                enableButton()
+                habilitarButton()
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -42,6 +57,7 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-
     }
+
+
 }
